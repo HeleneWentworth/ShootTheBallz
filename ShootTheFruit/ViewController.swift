53de6e72cc223e6c72f2f771100946bb57172f92
Helene <180022@virtualwindow.co.za>
2020-05-31 12:19:17 +0200

    Initial Commit

---
+++ ShootTheFruit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShootTheFruit
+//
+//  Created by Helene Wentworth on 2020/05/31.
+//  Copyright © 2020 Helene Wentworth. All rights reserved.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
