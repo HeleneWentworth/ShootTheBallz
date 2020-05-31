6b25908b9cc73646194da7a8cea832aaf4419361
Helene <180022@virtualwindow.co.za>
2020-06-01 00:41:25 +0200

    Custom Behaviour added
    USDZ object added
    Custom behaviour added.

--- ShootTheFruit/ViewController.swift
+++ ShootTheFruit/ViewController.swift
@@ -16,10 +16,34 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
         // Load the "Box" scene from the "Experience" Reality File
         let boxAnchor = try! Experience.loadBox()
         
         // Add the box anchor to the scene
         arView.scene.anchors.append(boxAnchor)
+        
+        //Custom Behaviour
+        boxAnchor.actions.showInterface.onAction = handleShowInterface(_:)
+       
+    }
+    
+    //MARK: - Custom Behaviour
+    
+    func handleShowInterface(_ entity: Entity?){
+        guard entity != nil else{
+            return
+        }
+         print("Scene Started")
+        
+    }
+    
+    //MARK: - Custom Trigger
+    func dropCans() {
+        if let sceneAnchor = arView.scene.anchors[0] as? Experience.Box {
+           
+            sceneAnchor.notifications.dropCans.post()
+            
+        }
     }
 }
